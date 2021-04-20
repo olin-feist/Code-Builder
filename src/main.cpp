fb77c505bc53f40d81f3f5d04d4bf433bfccedb0
Olin Feist <feisto@wwu.edu>
2021-04-19 19:26:31 -0700

    added new gui functionality

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,187 @@ void writeFile(vector<string> v){
     file.close();
 }
 
-int main(){
+
+class MyApp : public wxApp{
+    public:
+        virtual bool OnInit();
+};
+
+class MyFrame : public wxFrame{
+    public:
+        MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size);
+        wxPanel* panel_top;
+        
+
+    private:
+       
+        void OnQuit(wxCommandEvent &event);
+        void BuildMenu(wxCommandEvent &event);
+        
+        wxDECLARE_EVENT_TABLE();
+};
+
+class BuildFrame : public wxFrame{
+    public:
+        BuildFrame(const wxString &title, const wxPoint &pos, const wxSize &size);
+        wxPanel* panel_top;
+        wxBoxSizer *panelsizer;
+
+    private:
+        void OnFactory(wxCommandEvent &event);
+        void OnObserver(wxCommandEvent &event);
+
+        void choiceSelected(wxCommandEvent& event);
+        wxDECLARE_EVENT_TABLE();
+};
+
+enum{
+    ID_Factory = 1,
+    ID_Observer = wxID_HIGHEST + 1,
+    newcpp = wxID_HIGHEST + 1,
+    choiceevent =wxID_HIGHEST+1
+};
+
+wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
+    EVT_MENU(newcpp,  MyFrame::BuildMenu)
+    EVT_MENU(wxID_EXIT,  MyFrame::OnQuit)
+wxEND_EVENT_TABLE()
+
+
+wxBEGIN_EVENT_TABLE(BuildFrame, wxFrame)
+    EVT_BUTTON(ID_Factory, BuildFrame::OnFactory)
+    EVT_BUTTON(ID_Observer,  BuildFrame::OnObserver)
+    EVT_CHOICE(choiceevent,BuildFrame::choiceSelected)
+wxEND_EVENT_TABLE()
+
+wxIMPLEMENT_APP(MyApp);
+
+
+
+
+MyFrame::MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size): wxFrame(NULL, wxID_ANY, title, pos, size){
+    panel_top = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxSize(100, 100));
+    panel_top->SetBackgroundColour(wxColor(100, 100, 200));
+
+    wxPanel *panel_bottom = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxSize(100, 100));
+    panel_bottom->SetBackgroundColour(wxColor(100, 200, 100));
+
+   
+
+    wxBoxSizer *sizer = new wxBoxSizer(wxHORIZONTAL);
+    sizer->Add(panel_top, 1, wxEXPAND | wxALL, 10);
+    sizer->Add(panel_bottom, 1, wxEXPAND | wxALL, 10);
+
+    this->SetSizerAndFit(sizer);
+
+    wxBoxSizer *panelsizer = new wxBoxSizer(wxVERTICAL);
+
+
+
+    panel_top->SetSizerAndFit(panelsizer);
+    
+    wxMenuBar *menubar;
+    wxMenu *file;
+    wxMenuItem *quit;
+
+    menubar = new wxMenuBar;
+    file = new wxMenu;
+    wxMenu* newFile = new wxMenu;
+
+    //new file submenu
+    newFile->Append(newcpp, wxT("C++"));
+    newFile->Append(wxID_ANY, wxT("Java"));
+    newFile->Append(wxID_ANY, wxT("Python"));
+    file->AppendSubMenu(newFile, wxT("New File"));
+
+    file->Append(wxID_ANY, wxT("&Open"));
+    file->AppendSeparator();
+
+    quit = new wxMenuItem(file, wxID_EXIT, wxT("&Quit\tCtrl+W"));
+    file->Append(quit);
+
+    menubar->Append(file, wxT("&File"));
+    SetMenuBar(menubar);
+
+    SetClientSize(wxSize(1000,800));
+}
+
+BuildFrame::BuildFrame(const wxString &title, const wxPoint &pos, const wxSize &size): wxFrame(NULL, wxID_ANY, title, pos, size){
+    panel_top = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxSize(100, 100));
+    panel_top->SetBackgroundColour(wxColor(100, 100, 200));
+
+    wxBoxSizer *sizer = new wxBoxSizer(wxHORIZONTAL);
+    sizer->Add(panel_top, 1, wxEXPAND | wxALL, 10);
+    
+
+    this->SetSizerAndFit(sizer);
+
+    panelsizer = new wxBoxSizer(wxVERTICAL);
+
+    wxArrayString str;
+    str.Add("Factory");
+    str.Add("Observer");
+    wxChoice *wxCh = new wxChoice(panel_top,choiceevent, wxDefaultPosition, wxSize(100, 25),str);
+    wxCh->SetMaxSize(wxSize(100, 25));
+    
+    
+    panelsizer->Add(wxCh, 1, wxEXPAND | wxALL, 10);
+
+    panel_top->SetSizerAndFit(panelsizer);
+
+    SetClientSize(wxSize(400,400));
+}
+
+MyFrame *mainframe = new MyFrame("Code Builder", wxPoint(100, 100), wxSize(1100, 800));
+BuildFrame* Frame;
+
+
+
+void BuildFrame::OnFactory(wxCommandEvent &event){
+    cout<<"factory"<<endl;
+}
+
+void BuildFrame::OnObserver(wxCommandEvent& event){
+    cout<<"Observer"<<endl;
+}
+
+void MyFrame::OnQuit(wxCommandEvent& event){
+
+    Close(true);
+}
+
+void BuildFrame::choiceSelected(wxCommandEvent& event){
+    
+
+
+
+    wxButton *factorybutton = new wxButton(this->panel_top, ID_Factory, _T("Factory"),wxDefaultPosition,wxSize(100, 25), 0);
+    wxButton *observerbutton = new wxButton(this->panel_top, ID_Observer, _T("Observer"),wxDefaultPosition, wxSize(100, 25), 0);
+    
+    observerbutton->SetMaxSize(wxSize(100, 25));
+    factorybutton->SetMaxSize(wxSize(100, 25));
+    
+    this->panelsizer->Add(factorybutton, 1, wxEXPAND | wxALL, 10);
+    this->panelsizer->Add(observerbutton, 1, wxEXPAND | wxALL, 10);
+
+    panel_top->SetSizerAndFit(panelsizer);
+    
+    
+}
+
+void MyFrame::BuildMenu(wxCommandEvent& event){
+    Frame = new BuildFrame("C++ Builder", wxPoint(100, 100), wxSize(800, 800));
+    Frame->Show();
+}
+
+
+
+
+bool MyApp::OnInit(){
+    
+    mainframe->Show(true);
+    
+    /*
     cppbuilder build;
 
     string input;
@@ -80,70 +260,11 @@ int main(){
         cout<<"Subject Class Name"<<endl;
         cin>>classname;
         writeFile(build.observerbuilder(classname));
-    }
+    }*/
 
-    return 0;
-}
-
-class MyApp : public wxApp{
-    public:
-        virtual bool OnInit();
-};
-
-class MyFrame : public wxFrame{
-    public:
-        MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size);
-
-    private:
-        void OnHello(wxCommandEvent &event);
-        void OnExit(wxCommandEvent &event);
-        void OnAbout(wxCommandEvent &event);
-        wxDECLARE_EVENT_TABLE();
-};
-
-enum{
-    ID_Hello = 1
-};
-
-wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
-    EVT_MENU(ID_Hello, MyFrame::OnHello)
-        EVT_MENU(wxID_EXIT, MyFrame::OnExit)
-            EVT_MENU(wxID_ABOUT, MyFrame::OnAbout)
-                wxEND_EVENT_TABLE()
-                    wxIMPLEMENT_APP(MyApp);
-
-bool MyApp::OnInit(){
-    MyFrame *frame = new MyFrame("Hello World", wxPoint(50, 50), wxSize(450, 340));
-    frame->Show(true);
+    
     return true;
 }
 
-MyFrame::MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size): wxFrame(NULL, wxID_ANY, title, pos, size){
-    wxMenu *menuFile = new wxMenu;
-    menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
-                     "Help string shown in status bar for this menu item");
-    menuFile->AppendSeparator();
-    menuFile->Append(wxID_EXIT);
-    wxMenu *menuHelp = new wxMenu;
-    menuHelp->Append(wxID_ABOUT);
-    wxMenuBar *menuBar = new wxMenuBar;
-    menuBar->Append(menuFile, "&File");
-    menuBar->Append(menuHelp, "&Help");
-    SetMenuBar(menuBar);
-    CreateStatusBar();
-    SetStatusText("Welcome to wxWidgets!");
-}
 
-void MyFrame::OnExit(wxCommandEvent &event){
-    Close(true);
-}
-
-void MyFrame::OnAbout(wxCommandEvent &event){
-    wxMessageBox("This is a wxWidgets' Hello world sample",
-                 "About Hello World", wxOK | wxICON_INFORMATION);
-}
-
-void MyFrame::OnHello(wxCommandEvent &event){
-    wxLogMessage("Hello world from wxWidgets!");
-}
 
